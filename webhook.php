ef16e984eb013c0ca4073f8c0a55e39b95ddf9e7
ahmdsabbir <ahmd.sabbir@gmail.com>
2023-07-04 20:53:38 +0600

    update in webhook

--- webhook.php
+++ webhook.php
@@ -2,13 +2,21 @@
 // Trigger the webhook when a post is created or updated
 function webhook_notifier_send_data($post_ID) {
     $post = get_post($post_ID);
+    $api_key = get_option('linkages_api_key');
+    $domain = get_site_url();
+    $post_type = (get_post_type($post_ID) === 'post') ? 'post' : 'page';
 
     // Prepare the data to be sent as JSON
     $data = array(
         'id' => $post->ID,
         'url' => get_permalink($post->ID),
         'title' => $post->post_title,
-        'content' => $post->post_content
+        'content' => $post->post_content,
+        'status' => get_post_status($post),
+        'categories' => wp_get_post_categories($post->ID),
+        'api_key' => $api_key,
+        'domain' => $domain,
+        'post_type' => $post_type
     );
 
     // Convert data to JSON format
@@ -17,6 +25,7 @@ function webhook_notifier_send_data($post_ID) {
     // URL where the JSON data should be sent
     $webhook_url = 'http://127.0.0.1:5000/api/plugin/webhook';
 
+
     // Send the JSON data via a POST request
     $response = wp_remote_post($webhook_url, array(
         'body' => $json_data,
@@ -24,5 +33,6 @@ function webhook_notifier_send_data($post_ID) {
         'timeout' => 10,
         'sslverify' => false // Only if your webhook endpoint uses self-signed SSL certificate
     ));
+
 }
 add_action('save_post', 'webhook_notifier_send_data');
